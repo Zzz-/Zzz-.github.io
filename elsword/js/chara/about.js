fd6618fdf2adb3037ef254d40c22a6cdaec14fc2
solaireofzzz <pypynarwhal@gmail.com>
2014-01-29 11:58:50 +0900

    小魔追加、SA3種を更新、SA3種を再更新

--- elsword/js/chara/about.js
+++ elsword/js/chara/about.js
@@ -9,18 +9,15 @@ var About = function () {
 	  ]),
 
 	  new Job("更新履歴", "news", "n_sunny.png", [
+	    new Active("羅刹一式:吸魂", "Rakshasa Stance 1:Finger Bullet", 30, 0, 0, "", "Ara/Ara15_Finger Bullet.png", "SgCW50Y3HDQ"),
+	    new Active("ブレイズステップ", "Blaze Step", 60, 1, 0, "", "Aisha/Aisha12_Blaze Step.png", "mxoNISvfug0"),
+	    new Active("ライトニングシャワー", "Lightning Shower", 59, 1, 0, "", "Aisha/Aisha32_Lightning Shower.png", "OEryv5HrmGE"),
+	    new Active("パーフェクトストーム", "Perfect Storm", 36, 1, 0, "", "Rena/Rena03_Perfect Storm.png", "flaQhS8RmIE"),
+	    new Active("アイレリンナ－ニンフ", "Arielinna – Nymph", 42, -1, 0, "", "Rena/Rena14_Airelinna.png", "rXvsupWvQNA"),
+	    new Active("アイレリンナ－シルフ", "Arielinna – Sylph", 41, -1, 0, "", "Rena/Rena14a_Airelinna.png", "QHqjWaJJkpg"),
 	    new Active("ラシェのデータ", "Lacher", 0, -1, 0, "更新完了", "Lacher/Lacher.png"),
 	    new Active("ディメンションリンク－ブレーダー", "Dimension Link – Blader", 103, 1, 10, "", "Eve/Eve05_Dimension Link Blader.png", "EKigrq6oP3Q"),
 	    new Active("ウォーターキャノン", "Water Cannon", 61, 1, 0, "", "Aisha/Aisha31_Water Cannon.png", "FCGUR4F1ztk"),
-	    new Active("ライトニングシャワー", "Lightning Shower", 60, 1, 0, "", "Aisha/Aisha32_Lightning Shower.png", "ipHyIAyLewM"),
-	    new Active("シャイニングルーンバスター", "Shining Rune Buster", 67, 1, 0, "", "Els/ElsHA01_Shining Rune Buster.png", "GCm6AJhLnuY"),
-	    new Active("ギガンティックスラッシュ", "Gigantic Slash", 42, 1, 0, "", "Els/ElsHA02_Gigantic Slash.png", "adBxI1s8-ns"),
-			new Active("ブレードレイン", "Blade Rain", 148, 1, 0, "", "Els/ElsHA03_Blade Rain.png", "5Ob1WgqOYFc"),
-	    new Active("フェイトスペース", "Fate Space", 60, 1, 0, "", "Aisha/AishaHA03_Fate Space.png", "w5MLlXdKX2g"),
-	    new Active("エレメンタルストーム", "Elemental Storm", 89, 1, 0, "", "Aisha/AishaHA01_Elemental Storm.png", "cLfsXFCwPBo"),
-	    new Active("覇気天衝－千手華", "Thousand Blossoms", 96, 1, 0, "", "Ara/AraHA01_Thousand Blossoms.png", "huS3buJR31M"),
-	    new Active("レナのデータ", "Rena", 0, -1, 0, "26件更新", "Rena/Rena.png"),
-	    new Active("アビスアンゴル", "Abyss Angkor", 132, 0, 0, "吸引開始まで", "Aisha/AishaHA02_Abyss Angkor.png", "IIAanrKtI8Q"),
 	  ]),
 	  
 	];
