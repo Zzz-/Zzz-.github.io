b5a1135080e1e34c9f2a1a0b945be8e585094d24
solaireofzzz <pypynarwhal@gmail.com>
2014-02-18 04:04:44 +0900

    羅刹連携を更新、一部アイコンを変更

--- elsword/js/chara/about.js
+++ elsword/js/chara/about.js
@@ -9,6 +9,10 @@ var About = function () {
 	  ]),
 
 	  new Job("更新履歴", "news", "n_sunny.png", [
+	    new Active("羅刹二式:連環掌", "Rakshasa Stance 2: Energy Burst", 44, 1, 2, "", "Ara/Ara20_Energy Burst.png", "I2O9yFvq95A"),
+	    new Active("羅刹四式:奪命", "Rakshasa Stance 4: Soul Escape", 55, 2, 3, "", "Ara/Ara21_Soul Escape.png", "la-XuRiWkNM"),
+	    new Active("羅刹奥義:煉獄(単体)", "Secret Rakshasa Arts: Purgatory", 107, 1, 12, "", "Ara/AraEx04_Purgatory.png", "ti3tKD5x9Po"),
+	    new Active("羅刹奥義:煉獄(合計)", "Secret Rakshasa Arts: Purgatory (all)", 145, 1, 12, "奪命との合計", "Ara/AraEx04_Purgatory.png", ""),
 	    new Active("氣功波", "Energy Wave", 90, 1, 0, "", "Ara/Ara19_Energy Wave.png", "8ELFq7cCRVs"),
 	    new Active("鬼殺三式:影繋ぎ", "Exorcism 3: Shadow Knot", 78, 1, 0, "2回暗転スキル", "Ara/Ara06_Shadow Knot.png", "Bu87tiv3kcc"),
 	    new Active("(2回目の暗転)", "Exorcism 3: Shadow Knot", 98, 2, 1, "1Fだけ暗転無し", "Ara/Ara06_Shadow Knot.png", ""),
@@ -18,10 +22,6 @@ var About = function () {
 	    new Active("エルスのデータ", "Els", 0, -1, 0, "更新完了", "Els/Els.png"),
 	    new Active("アラのデータ", "Ara", 0, -1, 0, "15個更新", "Ara/Ara.png"),
 	    new Active("アイシャのデータ", "Aisha", 0, -1, 0, "残り1つ以外更新", "Aisha/Aisha.png"),
-	    new Active("狼牙四式:狼の牙", "Wolf Stance 4: Wolf Fang", 45, 1, 0, "", "Ara/Ara16_Wolf Fang.png", "1wORdpyUR6E"),
-	    new Active("連環獄", "Infernal Circle", 45, 1, 0, "", "Ara/Ara17_Infernal Circle.png", "GWftp2Mx1u8"),
-	    new Active("狼牙奥義:極砕", "Secret Wolf Arts: Killing Howl", 80, 1, 0, "連携中の暗転は全て途切れる", "Ara/AraEx03_Killing Howl.png", "MoqFc-V2rE4"),
-	    new Active("羅刹一式:吸魂", "Rakshasa Stance 1:Finger Bullet", 30, 0, 0, "", "Ara/Ara15_Finger Bullet.png", "SgCW50Y3HDQ"),
 	    new Active("ラシェのデータ", "Lacher", 0, -1, 0, "更新完了", "Lacher/Lacher.png"),
 	  ]),
 	  
